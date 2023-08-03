7efd8722725a4dc849dd8e5d152fa6b69f0769c5
Thiago Rodrigues <th14go@mail.com>
2023-08-03 13:21:26 -0300

    created project

---
+++ app/src/main/java/com/tibesoft/listsfinaceoffline/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tibesoft.listsfinaceoffline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
